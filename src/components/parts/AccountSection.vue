a6ee5604d0ba6b3b69d5f468ac0b99c70d94126f
dityaherdi <dityaherdi@gmail.com>
2019-12-02 10:28:39 +0700

    improve main page UI

--- src/components/parts/AccountSection.vue
+++ src/components/parts/AccountSection.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="uk-flex uk-flex-center animate uk-invisible">
-    <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=dityaherdi@gmail.com" target="_blank" uk-icon="icon: google" class="uk-icon-button"></a>
-    <a href="https://api.whatsapp.com/send?phone=6281338043380" target="_blank" uk-icon="icon: whatsapp" class="uk-margin-left uk-icon-button"></a>
-    <a href="https://github.com/dityaherdi" target="_blank" uk-icon="icon: github" class="uk-margin-left uk-icon-button"></a>
-    <a href="https://www.instagram.com/dityaherdi" target="_blank" uk-icon="icon: instagram" class="uk-margin-left uk-icon-button"></a>
-    <a href="https://www.facebook.com/herdinata.putra" target="_blank" uk-icon="icon: facebook" class="uk-margin-left uk-icon-button"></a>
-    <a href="https://www.linkedin.com/in/herdinata-putra-30bb4a180" target="_blank" uk-icon="icon: linkedin" class="uk-margin-left uk-icon-button"></a>
+  <div class="uk-flex uk-flex-center animate uk-invisible" uk-grid>
+    <div class="uk-width-1-2@s uk-width-1-1@l">
+      <a href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=dityaherdi@gmail.com" target="_blank" uk-icon="icon: google" class="uk-margin-right-small uk-icon-button"></a>
+      <a href="https://api.whatsapp.com/send?phone=6281338043380" target="_blank" uk-icon="icon: whatsapp" class="uk-margin-right-small uk-icon-button"></a>
+      <a href="https://github.com/dityaherdi" target="_blank" uk-icon="icon: github" class="uk-margin-right-small uk-icon-button"></a>
+      <a href="https://www.instagram.com/dityaherdi" target="_blank" uk-icon="icon: instagram" class="uk-margin-right-small uk-icon-button"></a>
+      <a href="https://www.facebook.com/herdinata.putra" target="_blank" uk-icon="icon: facebook" class="uk-margin-right-small uk-icon-button"></a>
+      <a href="https://www.linkedin.com/in/herdinata-putra-30bb4a180" target="_blank" uk-icon="icon: linkedin" class="uk-margin-right-small uk-icon-button"></a>
+    </div>
   </div>
 </template>
 
