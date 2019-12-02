c37aeb4c5da6cdfb50c390df140071d6a481bde9
dityaherdi <dityaherdi@gmail.com>
2019-12-02 17:04:21 +0700

    portfolio concept added

--- src/components/modals/PortfolioModal.vue
+++ src/components/modals/PortfolioModal.vue
@@ -2,7 +2,16 @@
   <div id="portfolio-modal" class="uk-modal-full" uk-modal>
     <div class="uk-modal-dialog" uk-overflow-auto uk-height-viewport>
         <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
-        <h1>PORTFOLIO COMING SOON...!</h1>
+        <div class="uk-container uk-padding">
+          <h3>Portfolio</h3>
+          <div class="uk-child-width-1-2 uk-child-width-1-4@s uk-grid-small uk-text-center" uk-grid="masonry: true">
+            <div uk-lightbox>
+              <a class="uk-button uk-button-default" data-type="image" href="https://i.scdn.co/image/07c323340e03e25a8e5dd5b9a8ec72b69c50089d" data-caption="Caption">
+              test
+              </a>
+            </div>
+          </div>
+        </div>
     </div>
   </div>
 </template>
